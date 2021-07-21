c9ad399f49b6c1643a478247a8897e70ec65ac9f
ZYROUGE <zyrouge@hotmail.com>
2021-07-21 11:49:41 +0530

    refactor: update url

--- guide.config.js
+++ guide.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     site_name: "Yukino - User Guide",
     base_url: "/",
-    github_url: "https://github.com/yukino-app/site",
+    github_url: "https://github.com/yukino-app/yukino-app",
 };
