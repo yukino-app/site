0c2d3015e33911b05685e251f55e321642c107a0
ZYROUGE <zyrouge@hotmail.com>
2021-11-05 12:23:40 +0530

    refactor: add patreon redirect

--- site/src/plugins/redirects.ts
+++ site/src/plugins/redirects.ts
@@ -6,6 +6,7 @@ export const redirects: [string, string][] = [
     ["/guide", URLs.wiki],
     ["/guides", URLs.wiki],
     ["/wiki", URLs.wiki],
+    ["/patreon", URLs.patreon],
 ];
 
 export const toRouteRecordRaw = (route: string, url: string) => {
