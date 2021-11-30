5a999963a38f34e5d3523d10381504f4471428dd
zyrouge <naveencnaveen96@gmail.com>
2021-11-30 12:14:22 +0000

    Deploying to gh-pages from @ yukino-app/yukino-app.github.io@15b25fdd0463d7939d0a3d8f82a76034540c3e3c 🚀

--- assets/404.187a9f37.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.d1206204.js";import{b as s,e as o,f as e}from"./vendor.1a903834.js";const c={},a=e("div",{class:"wx wy text-center"},[e("p",{class:"text-8xl md:text-9xl font-bold"},"404"),e("p",{class:"mt-8"}," Seems like someone has guided you to the wrong place. ")],-1),n=[a];function r(d,l){return s(),o("div",null,n)}var m=t(c,[["render",r]]);export{m as default};
