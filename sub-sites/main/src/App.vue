042107c5269695cd6c025e0acd9b00b91a3d76d8
zyrouge <naveencnaveen96@gmail.com>
2022-03-11 22:40:11 +0530

    chore: change url

--- sub-sites/main/src/App.vue
+++ sub-sites/main/src/App.vue
@@ -6,7 +6,7 @@ import NavBar from "./components/NavBar.vue";
     <!-- <NavBar /> -->
 
     <main class="font-sans flex justify-center items-center h-screen">
-        <p class="text-5xl font-bold drop-shadow-[3px_3px_rgba(203,203,203)]">
+        <p class="text-5xl font-bold drop-shadow-[3px_3px_rgba(203,203,203)] dark:drop-shadow-[3px_3px_rgba(76,76,76)]">
             Hang on till it's back.
         </p>
 
