f8f1986bcddd54e5809b06a4ae182deddbe94f75
zyrouge <naveencnaveen96@gmail.com>
2022-03-11 22:34:56 +0530

    chore: change url

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -3,7 +3,7 @@ import { Config } from "../config";
 const main = async () => {
     for (const site of Config.subSites) {
         console.log(`Processing: ${site.props.name}`);
-        await site.props.install?.();
+        await site.props.setup?.();
         console.log(`Processed: ${site.props.name}`);
     }
 };
